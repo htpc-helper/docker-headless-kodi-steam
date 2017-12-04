abe0ae5ab8ec7b89a9a1b6e94b449a1e7c386081
htpc-helper <htpc-helper@gmail.com>
2017-12-04 21:17:44 +0800

    change xfce4 startup

--- init/vnc-startup.sh
+++ init/vnc-startup.sh
@@ -2,31 +2,25 @@
 ### every exit != 0 fails the script
 set -e
 
+# Config variables
+USER=htpc-helper
+HOME="/home/$USER"
+PASSWD_PATH="$HOME/.vnc/passwd"
+
 ## resolve_vnc_connection
 VNC_IP=$(hostname -i)
 
 ## change vnc password
 echo -e "\n------------------ change VNC password  ------------------"
-# first entry is control, second is view (if only one is valid for both)
-mkdir -p "$HOME/.vnc"
-PASSWD_PATH="$HOME/.vnc/passwd"
+
 if [[ $VNC_VIEW_ONLY == "true" ]]; then
     echo "start VNC server in VIEW ONLY mode!"
     #create random pw to prevent access
     echo $(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 20) | vncpasswd -f > $PASSWD_PATH
 fi
 echo "$VNC_PW" | vncpasswd -f >> $PASSWD_PATH
+
 # Change ownership of documents dir
-chown -R $USER:$USER $HOME
+chown -R "$USER:$USER" $HOME
 chmod 600 $PASSWD_PATH
 
-# echo -e "\n------------------ startup of Xfce4 window manager ------------------"
-
-# ### disable screensaver and power management
-# xset -dpms &
-# xset s noblank &
-# xset s off &
-
-# /sbin/setuser htpc-helper /usr/bin/startxfce4 --replace > $HOME/wm.log &
-# sleep 1
-# cat $HOME/wm.log
